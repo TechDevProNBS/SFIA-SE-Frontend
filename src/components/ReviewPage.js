6f6afd2c1357ad9f7e365c81b9e9afd03bd0c2b3
james <jameswdemps@gmail.com>
2019-12-05 10:17:03 +0000

    Made the creation of lists work

--- src/components/ReviewPage.js
+++ src/components/ReviewPage.js
@@ -12,48 +12,96 @@ import "./css/ReviewPage.css";
 
 export default class ReviewPage extends React.Component {
   // Contains all information required for the slider to function
-  constructor(props){
+  constructor(props) {
     super(props);
+
+    this.state = {
+      remindIn: [
+        "1 Week",
+        "2 Weeks",
+        "3 Weeks",
+        "4 Weeks",
+        "5 Weeks",
+        "6 Weeks",
+        "7 Weeks",
+        "8 Weeks",
+        "3 Months",
+        "4 Months",
+        "5 Months",
+        "6 Months",
+        "7 Months",
+        "8 Months",
+        "9 Months",
+        "10 Months",
+        "11 Months",
+        "1 Year"
+      ],
+      customGoals: []
+    };
   }
 
-  state = {
-    remindIn: [
-      "1 Week",
-      "2 Weeks",
-      "3 Weeks",
-      "4 Weeks",
-      "5 Weeks",
-      "6 Weeks",
-      "7 Weeks",
-      "8 Weeks",
-      "3 Months",
-      "4 Months",
-      "5 Months",
-      "6 Months",
-      "7 Months",
-      "8 Months",
-      "9 Months",
-      "10 Months",
-      "11 Months",
-      "1 Year"
-    ],
+  deleteCustomGoal = input => {
+    var newCustomGoals = this.state.customGoals;
+    newCustomGoals.splice(input, 1);
+    this.setState({
+      customGoals: newCustomGoals
+    });
   };
+
+  customGoalsTextedit() {
+    var newText = document.getElementById("addCustomGoal").value;
+    this.setState({
+      customGoalsTextbox: newText
+    });
+  }
+
+  addCustomGoal() {
+    document
+      .getElementById("addCustomGoal")
+      .addEventListener("keypress", event => {
+        console.log("Keypress");
+        console.log(this.state.remindIn);
+        if (
+          event.keyCode == 13 &&
+          document.getElementById("addCustomGoal").value !== ""
+        ) {
+          event.preventDefault();
+          var newCustomGoals = this.state.customGoals;
+          newCustomGoals.push(document.getElementById("addCustomGoal").value);
+          document.getElementById("addCustomGoal").value = "";
+          this.setState({
+            customGoals: newCustomGoals
+          });
+        }
+      });
+  }
 
   // Updated remindInDate as the value of the slider to SessionWindow.js
   updateRemindInDate = () => {
-    this.props.updateRemindInDate(document.getElementById("slidecontainer").value)
+    this.props.updateRemindInDate(
+      document.getElementById("slidecontainer").value
+    );
   };
 
   // Updates the value of remindInDate to be the length of the slider on a first render in SessionWindow.js
   componentDidMount() {
-    this.props.updateRemindInDate(this.state.remindIn.length - 1)
+    this.props.updateRemindInDate(this.state.remindIn.length - 1);
+    document.getElementById("addCustomGoal").value = "";
+    this.setState({
+      customGoalsTextbox: ""
+    });
+    console.log(document.getElementById("addCustomGoal").value);
   }
 
   render() {
     // Determines the position of the words (date reminder set) underneath the slider
     var remindPosition = {
-      left: (this.props.remindInDate / (this.state.remindIn.length - 1)) * 100 - 3.5 + "%"
-    }
+      left:
+        (this.props.remindInDate / (this.state.remindIn.length - 1)) * 100 -
+        3.5 +
+        "%"
+    };
+
     return (
       <Container className="ReviewPage">
         <div>
@@ -174,45 +222,80 @@ export default class ReviewPage extends React.Component {
                 style={{ fontSize: 18 }}
               />
             </Form.Group>
-
-            <p class="font-weight-bold" style={{ fontSize: 20 }}>
-              <input
-                type="text"
-                placeholder="Add custom goal"
-                style={{ height: 30, width: 400, textAlign: "center" }}
-              />
-            </p>
-            <br></br>
-            <br></br>
-
-            <h4>
-              <p class="font-weight-bold" style={{ fontSize: 20 }}>
-                Set deadline for:
-              </p>
-            </h4>
-            <br></br>
-
-            {/* Code for the slider (date reminder) */}
-            <div className="slidersection">
-              <div>1 Week &nbsp;</div>
-              <div class="slidecontainer" className="slidecontainer">
-                <input
-                  type="range"
-                  min="0"
-                  max={this.state.remindIn.length - 1}
-                  // value="0"
-                  class="slider"
-                  id="slidecontainer"
-                  onChange={this.updateRemindInDate}
-                  className="slider"
-                />
-                <div style={remindPosition} className="slidecontainer">{this.state.remindIn[this.props.remindInDate]}</div>
-              </div>
-              <div>&nbsp; 1 Year</div>
-            </div>
-
-            <Button variant="primary">Submit</Button>
           </Form>
+
+          <table style={{ fontSize: 18 }}>
+            {this.state.customGoals.map((item, input) => {
+              var label = " " + item;
+              return (
+                <tr>
+                  <td>
+                    <input
+                      type="checkbox"
+                      label={label}
+                      style={{ fontSize: 18 }}
+                    />
+                  </td>
+                  <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                  <td>v)</td>
+                  <td>&nbsp;&nbsp;</td>
+                  <td>
+                    <div>{label}</div>
+                  </td>
+                  <td>
+                    <button
+                      id={input}
+                      onClick={() => this.deleteCustomGoal(input)}
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+
+          <p class="font-weight-bold" style={{ fontSize: 20 }}>
+            <input
+              type="text"
+              placeholder="Add custom goal"
+              style={{ height: 30, width: 400, textAlign: "center" }}
+              id="addCustomGoal"
+              onKeyDown={() => this.addCustomGoal()}
+            />
+          </p>
+          <br></br>
+          <br></br>
+
+          <h4>
+            <p class="font-weight-bold" style={{ fontSize: 20 }}>
+              Set deadline for:
+            </p>
+          </h4>
+          <br></br>
+
+          {/* Code for the slider (date reminder) */}
+          <div className="slidersection">
+            <div>1 Week &nbsp;</div>
+            <div class="slidecontainer" className="slidecontainer">
+              <input
+                type="range"
+                min="0"
+                max={this.state.remindIn.length - 1}
+                // value={this.state.customGoalsTextbox}
+                class="slider"
+                id="slidecontainer"
+                onChange={this.updateRemindInDate}
+                className="slider"
+              />
+              <div style={remindPosition} className="slidecontainer">
+                {this.state.remindIn[this.props.remindInDate]}
+              </div>
+            </div>
+            <div>&nbsp; 1 Year</div>
+          </div>
+
+          <Button variant="primary">Submit</Button>
         </div>
       </Container>
     );
