599f0bdde97a40df4bcde262ae1a296483ca9df3
Manbir <mbhullar96@hotmail.com>
2019-12-05 16:55:13 +0000

    Mapped data from database and enable/disable dropdown

--- src/components/Responsibilities.js
+++ src/components/Responsibilities.js
@@ -1,263 +1,136 @@
 import React from 'react';
-import { Container, Row, Col, Form, Card, ListGroup } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './css/Responsibilities.css'
 
 export default class Responsibilities extends React.Component {
   constructor() {
     super();
     this.state = {
-      resp: [
-        {
-          "responsibility_name": "Autonomy",
-          "responsibility_descriptions": [
-            {
-              "level": 1,
-              "responsibility_criteria": [
-                {
-                  "responsibility_criterion": "Works under supervision."
-                },
-                {
-                  "responsibility_criterion": "Uses little discretion."
-                },
-                {
-                  "responsibility_criterion": "Is expected to seek guidance in unexpected situations."
-                }
-              ]
-            },
-            {
-              "level": 2,
-              "responsibility_criteria": [
-                {
-                  "responsibility_criterion": "Works under routine direction."
-                },
-                {
-                  "responsibility_criterion": "Uses limited discretion in resolving issues or enquiries."
-                },
-                {
-                  "responsibility_criterion": "Works without frequent reference to others."
-                }
-              ]
-            },
-            {
-              "level": 3,
-              "responsibility_criteria": [
-                {
-                  "responsibility_criterion": "Works under general direction."
-                },
-                {
-                  "responsibility_criterion": "Uses discretion in identifying and responding to complex issues and assignments."
-                },
-                {
-                  "responsibility_criterion": "Receives specific direction, accepts guidance and has work reviewed at agreed milestones. Determines when issues should be escalated to a higher level."
-                }
-              ]
-            },
-            {
-              "level": 4,
-              "responsibility_criteria": [
-                {
-                  "responsibility_criterion": "Works under general direction within a clear framework of accountability."
-                },
-                {
-                  "responsibility_criterion": "Exercises substantial personal responsibility and autonomy."
-                },
-                {
-                  "responsibility_criterion": "Plans own work to meet given objectives and processes."
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "responsibility_name": "Influence",
-          "responsibility_descriptions": [
-            {
-              "level": 1,
-              "responsibility_criteria": [
-                {
-                  "responsibility_criterion": "Minimal influence."
-                },
-                {
-                  "responsibility_criterion": "May work alone, or interact with immediate colleagues."
-                }
-              ]
-            },
-            {
-              "level": 2,
-              "responsibility_criteria": [
-                {
-                  "responsibility_criterion": "Interacts with and may influence immediate colleagues."
-                },
-                {
-                  "responsibility_criterion": "May have some external contact with customers, suppliers and partners."
-                },
-                {
-                  "responsibility_criterion": "May have more influence in own domain."
-                },
-                {
-                  "responsibility_criterion": "Aware of need to collaborate with team and represent users/customer needs."
-                }
-              ]
-            }
-          ]
-        }
-      ],
+      resp: [],
 
       lblDefault: "Select",
       lblYes: "Yes",
       lblNo: "No",
-      disableLvl2: true,
-      disableLvl3: true,
-      disableLvl4: true,
-      disableLvl5: true,
-      disableLvl6: true,
-      disableLvl7: true
+      visible: "visible",
+      disable: true
     }
-    // tableData = () => {
-    //     return this.state.resp.map(resName => {
-    //         return (
-    //             <tr>
-    //                 <td className="headcol">{resName.responsibility_name}</td>
-    //                 {resName.responsibility_descriptions.map(resDesc => {
-    //                     {resDesc.responsibility_criteria.map(resCrit => {
-
-    //                     })}
-    //                 })}
-    // <td id="lvl1">
-    //     <span style={{ display: "inline-flex" }}>
-    //         <select id="sel" onChange={() => this.count1(1)}>
-    //             <option name="checked" value="select">{this.state.lblDefault}</option>
-    //             <option value="true">{this.state.lblYes}</option>
-    //             <option value="false">{this.state.lblNo}</option>
-    //         </select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    //         <p>{item.lvl1}</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    //     </span>
-    // </td>
-    //             </tr>
-    //         )
-    //     })
-    // }
+  }
+  getResp = () => {
+    fetch(`http://localhost:8084/API/showResponsibilities`)
+      .then(response => response.json())
+      .then(responsibility_info => this.setState({ resp: responsibility_info }))
   }
 
+  componentDidMount = () => {
+    this.getResp();
+  }
+  componentDidUpdate = () => {
+    var select = document.getElementsByName("0");
+    for (var i = 0; i < select.length; i++) {
+      console.log(select[i]);
+      select[i].disabled = false;
+    }
+  }
 
-  count1 = (index) => {
+  enableSelect = (input) => {
+    var select = document.getElementsByName(input);
+    for (var i = 0; i < select.length; i++) {
+      console.log(select[i] + " enable");
+      select[i].disabled = false;
+    }
+  }
+
+  disableSelect = (input) => {
+    var select = document.getElementsByName(input);
+    for (var i = 0; i < select.length; i++) {
+      console.log(select[i]);
+      select[i].disabled = true;
+      console.log(select[i].value);
+      
+    }
+  }
+
+  selectCount = (index) => {
     var tab = document.getElementById("list");
+    var select = document.getElementById(index);
+    console.log(select);
     var row = tab.rows.length;
     var tr, td;
     var count = 0;
     var per = 0;
-    // if (index < 0) {
-    //   return null;
-    // }
-    var result = [];
-    for (var i = 0, j = 0; i < row; i++) {
-      for (j = 1; j < row; j++) {
-        result.push(tab.rows[i].cells[index+1]);
-        console.log(tab.rows[i].cells[index+1]);
-        
+    var total = 0;
+    for (var i = 0; i < row; i++) {
+      tr = tab.rows[i];
+      td = tr.cells[index + 1];
+      var a = td.getElementsByTagName("span");
+      for (var j = 0; j < a.length; j++) {
+        if (a[j].firstElementChild.value == "true") {
+          count++;
+        }
+        total++;
       }
     }
-    console.log(result.length);
-    
-    // for (var i = 0; i < row; i++) {
-    //   tr = tab.rows[i];
-    //   console.log(tr);
-    //   td = tr.cells[index + 1];
-    //   console.log(td);
-    //   var a = td.getElementsByTagName("span");
-    //   console.log(a.length);
-    //   for (var j = 0; j < a.length; j++) {
-    //     if (a[j].firstElementChild.value == "true") {
-    //       count++;
-    //     }
-    //   }
-    // }
-    
-    // console.log(count);
-    // per = ((count / a.length) * 100).toFixed(2);
-    // console.log(per);
-    // if (per > ((2 / 3) * 100)) {
-    //   switch (col) {
-    //     case 1:
-    //       this.setState({
-    //         disableLvl2: false,
-    //       })
-    //       break;
-    //     case 2:
-    //       this.setState({
-    //         disableLvl3: false
-    //       })
-    //       break;
-    //     case 3:
-    //       this.setState({
-    //         disableLvl4: false
-    //       })
-    //       break;
-    //     case 4:
-    //       this.setState({
-    //         disableLvl5: false
-    //       })
-    //       break;
-    //     case 5:
-    //       this.setState({
-    //         disableLvl6: false
-    //       })
-    //       break;
-    //     case 6:
-    //       this.setState({
-    //         disableLvl7: false
-    //       })
-    //       break;
-    //   }
-    // } else {
-    //   switch (col) {
-    //     case 1:
-    //       this.setState({
-    //         disableLvl2: true,
-    //         disableLvl3: true,
-    //         disableLvl4: true,
-    //         disableLvl5: true,
-    //         disableLvl6: true,
-    //         disableLvl7: true
-    //       })
-    //       break;
-    //     case 2:
-    //       this.setState({
-    //         disableLvl3: true,
-    //         disableLvl4: true,
-    //         disableLvl5: true,
-    //         disableLvl6: true,
-    //         disableLvl7: true
-    //       })
-    //       break;
-    //     case 3:
-    //       this.setState({
-    //         disableLvl4: true,
-    //         disableLvl5: true,
-    //         disableLvl6: true,
-    //         disableLvl7: true
-    //       })
-    //       break;
-    //     case 4:
-    //       this.setState({
-    //         disableLvl5: true,
-    //         disableLvl6: true,
-    //         disableLvl7: true
-    //       })
-    //       break;
-    //     case 5:
-    //       this.setState({
-    //         disableLvl6: true,
-    //         disableLvl7: true
-    //       })
-    //       break;
-    //     case 6:
-    //       this.setState({
-    //         disableLvl7: true
-    //       })
-    //       break;
-    //   }
-    // }
+
+    per = ((count / total) * 100).toFixed(2);
+    console.log(per + "%");
+    if (per > ((2 / 3) * 100)) {
+      switch (index) {
+        case 0:
+          this.enableSelect(1);
+          break;
+        case 1:
+          this.enableSelect(2);
+          break;
+        case 2:
+          this.enableSelect(3);
+          break;
+        case 3:
+          this.enableSelect(4);
+          break;
+        case 4:
+          this.enableSelect(5);
+          break;
+        case 5:
+          this.enableSelect(6);
+          break;
+      }
+    } else {
+      switch (index) {
+        case 0:
+          this.disableSelect(1);
+          this.disableSelect(2);
+          this.disableSelect(3);
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 1:
+          this.disableSelect(2);
+          this.disableSelect(3);
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 2:
+          this.disableSelect(3);
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 3:
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 4:
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 5:
+          this.disableSelect(6);
+          break;
+      }
+    }
   }
 
   render() {
@@ -278,15 +151,14 @@ export default class Responsibilities extends React.Component {
               </tr>
             </thead>
             <tbody id="list">
-              {/* {this.tableData()} */}
               {this.state.resp.map((name, index) => (
                 <tr>
                   <td><b>{name.responsibility_name}</b></td>
                   {name.responsibility_descriptions.map((desc, index) => (
                     <td>
                       {desc.responsibility_criteria.map((desc2, index2) => (
-                        <span>
-                          <select id="sel" onChange={() => this.count1(index)}>
+                        <span style={{ visibility: this.state.visible }}>
+                          <select id={index2} name={index} disabled={this.state.disable} onChange={() => this.selectCount(index)}>
                             <option name="checked" value="select">{this.state.lblDefault}</option>
                             <option value="true">{this.state.lblYes}</option>
                             <option value="false">{this.state.lblNo}</option>
@@ -296,10 +168,8 @@ export default class Responsibilities extends React.Component {
                       ))}
                     </td>
                   ))}
-
                 </tr>
               ))}
-
             </tbody>
           </table>
         </div>
--- src/components/SkillLevel.js
+++ src/components/SkillLevel.js
@@ -1,7 +1,174 @@
-import React from "react";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './css/Responsibilities.css'
 
-export default class SkillList extends React.Component {
+export default class SkillLevel extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skillLvl: [],
+      
+      lblDefault: "Select",
+      lblYes: "Yes",
+      lblNo: "No",
+      disable: true
+    }
+  }
+  getResp = () => {
+    fetch(`http://localhost:8084/API/showResponsibilities`)
+      .then(response => response.json())
+      .then(responsibility_info => this.setState({ resp: responsibility_info }))
+  }
+
+  componentDidUpdate = () => {
+    var select = document.getElementsByName("0");
+    for (var i = 0; i < select.length; i++) {
+      console.log(select[i]);
+      select[i].disabled = false;
+    }
+  }
+
+  enableSelect = (input) => {
+    var select = document.getElementsByName(input);
+    for (var i = 0; i < select.length; i++) {
+      console.log(select[i] + " enable");
+      select[i].disabled = false;
+    }
+  }
+
+  disableSelect = (input) => {
+    var select = document.getElementsByName(input);
+    for (var i = 0; i < select.length; i++) {
+      console.log(select[i] + " disable");
+      select[i].disabled = true;
+    }
+  }
+
+  selectCount = (index) => {
+    var tab = document.getElementById("list");
+    var select = document.getElementById(index);
+    console.log(select);
+    var row = tab.rows.length;
+    var tr, td;
+    var count = 0;
+    var per = 0;
+    var total = 0;
+    var ttlPer = 0;
+    for (var i = 0; i < row; i++) {
+      tr = tab.rows[i];
+      td = tr.cells[index + 1];
+      var a = td.getElementsByTagName("span");
+      for (var j = 0; j < a.length; j++) {
+        if (a[j].firstElementChild.value == "true") {
+          count++;
+        }
+        total++;
+      }
+    }
+
+    per = ((count / total) * 100).toFixed(2);
+    console.log(per + "%");
+    if (per > ((2 / 3) * 100)) {
+      switch (index) {
+        case 0:
+          this.enableSelect(1);
+          break;
+        case 1:
+          this.enableSelect(2);
+          break;
+        case 2:
+          this.enableSelect(3);
+          break;
+        case 3:
+          this.enableSelect(4);
+          break;
+        case 4:
+          this.enableSelect(5);
+          break;
+        case 5:
+          this.enableSelect(6);
+          break;
+      }
+    } else {
+      switch (index) {
+        case 0:
+          this.disableSelect(1);
+          this.disableSelect(2);
+          this.disableSelect(3);
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 1:
+          this.disableSelect(2);
+          this.disableSelect(3);
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 2:
+          this.disableSelect(3);
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 3:
+          this.disableSelect(4);
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 4:
+          this.disableSelect(5);
+          this.disableSelect(6);
+          break;
+        case 5:
+          this.disableSelect(6);
+          break;
+      }
+    }
+  }
+
   render() {
-    return <div>It's skillLevel</div>;
+    return (
+      <Container>
+        <div className="lvls">
+          <table id="table" class="table table-hover">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Level 1</th>
+                <th>Level 2</th>
+                <th>Level 3</th>
+                <th>Level 4</th>
+                <th>Level 5</th>
+                <th>Level 6</th>
+                <th>Level 7</th>
+              </tr>
+            </thead>
+            <tbody id="list">
+              {this.state.skillLvl.map((name, index) => (
+                <tr>
+                  <td><b>{name.responsibility_name}</b></td>
+                  {name.responsibility_descriptions.map((desc, index) => (
+                    <td>
+                      {desc.responsibility_criteria.map((desc2, index2) => (
+                        <span style={{ visibility: this.state.visible }}>
+                          <select id={index2} name={index} disabled={this.state.disable} onChange={() => this.selectCount(index)}>
+                            <option name="checked" value="select">{this.state.lblDefault}</option>
+                            <option value="true">{this.state.lblYes}</option>
+                            <option value="false">{this.state.lblNo}</option>
+                          </select>
+                          <p>{desc2.responsibility_criterion}</p><br />
+                        </span>
+                      ))}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </Container>
+    );
   }
 }
